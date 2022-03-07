f8b9a6ee771b6a5fbab38fa59f841ed088709987
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-03-06 22:40:33 -0300

    Atualização

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform player;
+    public float camSpeed;
+    public float offSet;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        Vector3 newCamPos = new Vector3(player.position.x + offSet, 0f, transform.position.z);
+        transform.position = Vector3.Slerp(transform.position, newCamPos, camSpeed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D rig;
+    private Rigidbody2D rig;
+    private bool isJumping;
     public Animator anim;
     public float speed;
     public float jump;
@@ -12,7 +13,6 @@ public class Player : MonoBehaviour
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -27,16 +27,25 @@ public class Player : MonoBehaviour
 
     void Move()
     {
-        //anim.SetInteger("transicao", 3);
         rig.velocity = new Vector2(speed, rig.velocity.y);
     }
 
     void Jump()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && !isJumping)
         {
-            //anim.SetInteger("transicao", 2);
+            anim.SetBool("Jumping", true);
             rig.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+            isJumping = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            anim.SetBool("Jumping", false);
+            isJumping = false;
         }
     }
 }
