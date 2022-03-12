e4034060841c0512de43ddde4c0b6153c970ceef
Miller Vasco <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2022-03-12 11:27:12 -0300

    Update GameControllerUI.cs

--- Assets/Scripts/GameUI/GameControllerUI.cs
+++ Assets/Scripts/GameUI/GameControllerUI.cs
@@ -16,17 +16,31 @@ public class GameControllerUI : MonoBehaviour
     private void Awake()
     {
         instance = this;
+        
+        if(PlayerPrefs.GetInt("Moedas") > 0)
+        {
+            score += PlayerPrefs.GetInt("Moedas");
+            scoreText.text = "x " + score.ToString();
+        }
+        
+        if(PlayerPrefs.GetInt("Points") > 0)
+        {
+            scorePoints += PlayerPrefs.GetInt("Points");
+            TextPoints.text = "x " + TextPoints.ToString();
+        }
     }
 
     public void GetCoin()
     {
         score++;
         ScoreText.text = "x " + score.ToString();
+        PlayerPrefs.SetInt("Moedas", score);
     }
 
     public void GetPoints()
     {
         scorePoints += 100;
         TextPoints.text = scorePoints.ToString();
+        PlayerPrefs.SetInt("Points", scorePoints);
     }
 }
